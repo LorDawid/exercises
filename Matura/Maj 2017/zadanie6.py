783f70f1863f3dd2902bc0632210e3bbb84e907f
LorDawid <szostak.d7@gmail.com>
2023-03-05 15:58:17 +0100

    zadanie 6.3 matura maj

--- Matura/Maj 2017/zadanie6.py
+++ Matura/Maj 2017/zadanie6.py
@@ -4,7 +4,8 @@ with open("dane.txt") as file:
     for info in file.readlines():
         imageData.append([int(x) for x in info.strip().split()])
 
-def ex6_1():
+def ex6_1() -> tuple[int, int]:
+    #Podaj jasność najjaśniejszego i jasność najciemniejszego piksela. 
     darkest, lightest = 0, 0
     for line in imageData:
         for value in line:
@@ -18,7 +19,8 @@ def ex6_1():
 def isSymmetrical(linia: list[int]) -> bool:
     return linia[::-1] == linia
 
-def ex6_2():
+def ex6_2() -> int:
+    #Podaj, ile wynosi najmniejsza liczba wierszy, które należy usunąć, żeby obraz miał pionową oś symetrii.
     nonSymmetrical = 0
     for line in imageData:
         if not isSymmetrical(line):
@@ -28,3 +30,35 @@ def ex6_2():
 
 # print(f"Aby obraz byl symetryczny trzeba usunac {ex6_2()} linijek")
 
+def getNeighbours(x: int, y: int) -> bool:
+    #jestem bardzo dumny z tego rozwiazania, jest calkiem szybkie
+    #przechodzimy przez kazdy piksel i bierzemy wartosci pikseli
+    #po jego lewej prawej gorze i dole, clampujemy te wartosci do
+    #maksymalnych wartosci obrazu, zeby nie bylo IndexErroru na kazdym
+    #brzegowym pikselu, jesli roznica miedzy ktoryms z pikseli
+    #jest wieksza niz 128, zwracamy prawde.
+
+    currentPixel = imageData[y][x]
+    neighbours = [
+        imageData[y][min(x+1, 319)],
+        imageData[y][max(x-1, 0  )],
+        imageData[max(y-1, 0  )][x],
+        imageData[min(y+1, 199)][x],
+    ]
+
+    for neighbour in neighbours:
+        if abs(neighbour - currentPixel) > 128:
+            return True
+
+    return False
+
+def ex6_3() -> int:
+    #Podaj liczbę wszystkich takich pikseli, dla których istnieje przynajmniej jeden kontrastujący z nim sąsiedni piksel. 
+    contrasting = 0
+    for y, line in enumerate(imageData):
+        for x in range(len(line)):
+            contrasting += getNeighbours(x, y)
+
+    return contrasting
+
+print(f"Liczba kontrastujacych pikseli: {ex6_3()}")
