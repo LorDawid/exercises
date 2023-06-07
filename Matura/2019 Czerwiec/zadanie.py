856c1fa5b7cd61572e2184efa896ea42e5ef2a60
Dawid Sz <44745662+LorDawid@users.noreply.github.com>
2023-06-07 08:44:50 +0200

    Add files via upload

--- Matura/2019 Czerwiec/zadanie.py
+++ Matura/2019 Czerwiec/zadanie.py
@@ -32,9 +32,21 @@ def sumNumberElements(number: int) -> int:
 
 def ex_4_3():
     #zwraca dlugosc listy liczb ktore od tylu maja sume = 1
-    return len([x for x in primes if sumNumberElements(int(str(x)[::-1])) == 1])
+    return len([x for x in primes if sumNumberElements(x) == 1]) # Nie ma sensu liczyć dla liczb, które czytamy od tyłu. Wynik jest ten sam, ale w innym zadaniu taki błąd mógłby wpłynąc na poprawność odpowiedzi
 
-print(ex_4_1()) #zadanie 1
-print(ex_4_2()) #zadanie 2
-print(ex_4_3()) #zadanie 3
+ex1 = ",\n".join(str(x) for x in ex_4_1()) #zadanie 1
+ex2 = ",\n".join(str(x) for x in ex_4_2()) #zadanie 2
+ex3 = ex_4_3() #zadanie 3
 #czas wykonanie: 20 minut
+
+with open("wyniki1.txt", "w") as file:
+    file.write(f"""
+Zad1:
+{ex1}
+
+Zad2:
+{ex2}
+
+Zad3:
+{ex3}
+""")
